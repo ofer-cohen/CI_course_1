73736a9d6cb8d43d4c3eb034e8fed5f1dbf2588a
ofer-cohen <ofer.cohen@reuben.ox.ac.uk>
2025-10-20 12:12:38 +0100

    Update test_functionality.py

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -13,6 +13,7 @@ def test_minimum():
     """
     Test the function `minimum` in functionality.py
     """
+    assert ci_course.minimum("hi", "there") is None
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
     assert ci_course.minimum(-1.2, -3) == -3
